01bdb4acbd6b021da1b4720a0e2711ff42d01d2c
NemoNology <chicken.gang@mail.ru>
2023-02-19 16:58:07 +0900

    Added IP Calculator Solution

---
+++ Lab-s/4/Solution/IP calculator (WF)/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace IP_calculator__WF_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Calculate available information by IP and Mask 
+        /// <br/> And Cut Network to SubNetworks, if possible
+        /// </summary>
+        private void ButtonInput_Click(object sender, EventArgs e)
+        {
+            status.Text = "...";
+
+            if (inputMask.Value > 32 || inputMask.Value < 0)
+            {
+                status.Text = "Incorrect bitMask";
+                return;
+            }
+
+            try
+            {
+                Network.IPString = inputIP.Text.Replace(" ", "");
+                Network.BitMask = (int)inputMask.Value;
+            }
+            catch (Exception exc)
+            {
+                status.Text = exc.Message;
+                return;
+            }
+
+            outputIP.Text = Network.IPString;
+            outputMask.Text = Network.BitMask.ToString();
+            outputNetworkClass.Text = Network.NetworkClass;
+            outputNetworkAddress.Text = Network.Address;
+            outputBroadcast.Text = Network.Broadcast;
+            outputNetworkMask.Text = Network.NetworkMaskString;
+            outputWildMask.Text = Network.WildMaskString;
+            outputHostsAmount.Text = Network.HostsAmount.ToString() + " + 2 (Broadcast and Network Address)";
+            outputFirstAddress.Text = Network.FirstAddress;
+            outputLastAddress.Text = Network.LastAddress;
+
+
+            if (inputSubnetworks.Text.Length != 0)
+            {
+                string[] buffer = inputSubnetworks.Text.Split(' ');
+                uint[] numbers = new uint[buffer.Length]; 
+
+                try
+                {
+                    for (int i = 0; i < buffer.Length; i++)
+                    {
+                        numbers[i] = Convert.ToUInt32(buffer[i]);
+                    }
+                }
+                catch
+                {
+                    status.Text = "Incorrect value in SubNetworks field";
+                    return;
+                }
+
+                numbers = Array.FindAll<uint>(numbers, x => x != 0);
+
+                uint sum = 0;
+
+                foreach (var item in numbers)
+                {
+                    sum += (uint)Math.Pow(2, Math.Ceiling(Math.Log(item, 2)));
+                }
+
+                if (sum > Network.HostsAmount)
+                {
+                    status.Text = "Needed hosts amount more that there is in given Network";
+                    return;
+                }
+
+                Array.Sort(numbers);
+                Array.Reverse(numbers);
+
+                outputSubnetworks.Rows.Clear();
+
+                Network.IPString = Network.Address;
+
+                foreach (var item in numbers)
+                {
+                    int bitMask = Network.BitMask;
+
+                    while (item < (uint)(Math.Pow(2, Network.DigitsAmountInIP - bitMask) - 2))
+                    {
+                        bitMask++;
+                    }
+
+                    bitMask--;
+
+                    Network.BitMask = bitMask;
+
+                    outputSubnetworks.Rows.Add(
+                        Network.IPString,
+                        Network.NetworkMaskString + " / " + bitMask.ToString(),
+                        item,
+                        Network.HostsAmount,
+                        Network.HostsAmount - item,
+                        Network.FirstAddress + " - " + Network.LastAddress,
+                        Network.Address,
+                        Network.Broadcast
+                        );
+
+                    Network.IPString = Network.Broadcast;
+
+                    Network.IPInt += 1;
+                }
+            }
+        }
+
+        private void HotKeys(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return ||
+                e.KeyCode == Keys.Enter)
+            {
+                ButtonInput_Click(sender, e);
+            }
+        }
+
+        /// <summary>
+        /// Calculating when inputMask.Value Changed
+        /// </summary>
+        private void FastTravel(object sender, EventArgs e)
+        {
+            if (inputMask.Value == 99)
+            {
+                inputIP.Text = "19216811 2";
+                inputMask.Value = 24;
+                inputSubnetworks.Text = "100 60 60 40 20";
+            }
+        }
+
+        /// <summary>
+        /// Generate random values for IP and Mask
+        /// </summary>
+        private void ButtonRandom_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+
+            Network.IPInt = rnd.Next(int.MinValue, int.MaxValue);
+            Network.BitMask = rnd.Next(0, 32);
+
+            inputIP.Text = Network.IPStringWithSpaces;
+            inputMask.Value = Network.BitMask;
+
+            ButtonInput_Click(sender, e);
+        }
+    }
+}
---
+++ Lab-s/4/Solution/IP calculator (WF)/Network.cs
@@ -0,0 +1,460 @@
+﻿using System;
+
+namespace IP_calculator__WF_
+{
+    class Network
+    {
+        #region Fields & Properties
+
+        private static bool _isIPv6 = false;
+        private static byte[] _ip = new byte[4];
+        private static byte[] _mask = new byte[4];
+        public static readonly int OktetSize = 8;
+
+        /// <summary>
+        /// Return true if it's IPv6, false - IPv4
+        /// </summary>
+        public static bool VersionOfIP
+        {
+            get { return _isIPv6; }
+            set { _isIPv6 = value; }
+        }
+
+        /// <summary>
+        /// Return digits amount in IP (32 for IPv4, 48 - IPv6)
+        /// </summary>
+        public static int DigitsAmountInIP => OktetSize * (_isIPv6 ? 6 : 4);
+
+        /// <summary>
+        /// IP Address
+        /// </summary>
+        public static byte[] IP
+        {
+            get
+            {
+                return _ip;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(IP), $"{nameof(IP)} can't be null");
+                }
+                else if (value.Length != _ip.Length)
+                {
+                    throw new ArgumentException($"The inputed value length can't be different from the Address IP ({_ip.Length})", nameof(IP));
+                }
+
+                _ip = value;
+            }
+        }
+
+        /// <summary>
+        /// IP Address as string <br/>
+        /// <example>  <b> Examples: </b> <br/> <i> 192.168.11.1 <br/> 0.0.12.0 </i>  </example>
+        /// </summary>
+        public static string IPString
+        {
+            get
+            {
+                string res = string.Empty;
+
+                foreach (var item in _ip)
+                {
+                    res += item.ToString() + ".";
+                }
+
+                return res.Substring(0, res.Length - 1);
+            }
+
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(nameof(IPString), $"{nameof(IPString)} can't be null");
+                }
+
+                var buffer = value.Split('.', ',', ' ');
+
+                if (buffer.Length != _ip.Length)
+                {
+                    throw new ArgumentException($"{nameof(IPString)} has incorrect length", nameof(IPString));
+                }
+
+                byte[] res = new byte[_ip.Length];
+
+                for (int i = 0; i < _ip.Length; i++)
+                {
+                    if (!Byte.TryParse(buffer[i], out res[i]))
+                    {
+                        throw new ArgumentException($"{nameof(IPString)} has incorrect value", nameof(IPString));
+                    }
+                }
+
+                _ip = res;
+            }
+        }
+
+        /// <summary>
+        /// Network Mask
+        /// </summary>
+        public static byte[] NetworkMask
+        {
+            get { return _mask; }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(NetworkMask), $"{nameof(NetworkMask)} can't be null");
+                }
+                else if (value.Length != NetworkMask.Length)
+                {
+                    throw new ArgumentException($"The inputed value length can't be different from the Network Mask ({_mask.Length})", nameof(value));
+                }
+
+                string buffer = string.Empty;
+
+                foreach (var item in value)
+                {
+                    buffer += Convert.ToString(item, 2).PadRight(OktetSize, '0');
+                }
+
+                if (buffer.LastIndexOf("1") > buffer.IndexOf("0"))
+                {
+                    throw new ArgumentException($"The {nameof(NetworkMask)} was incorrect", nameof(NetworkMask));
+                }
+
+                _mask = value;
+            }
+        }
+
+        /// <summary>
+        /// Network Mask as string
+        /// </summary>
+        public static string NetworkMaskString
+        {
+            get
+            {
+                string res = string.Empty;
+
+                foreach (var item in _mask)
+                {
+                    res += item.ToString() + ".";
+                }
+
+                return res.Substring(0, res.Length - 1);
+            }
+
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(nameof(NetworkMaskString), $"{nameof(NetworkMaskString)} can't be null");
+                }
+
+                var buffer = value.Split('.', ',', ' ');
+
+                if (buffer.Length != _mask.Length)
+                {
+                    throw new ArgumentException($"{nameof(NetworkMaskString)} has incorrect length", nameof(NetworkMaskString));
+                }
+
+                byte[] res = new byte[_mask.Length];
+
+                for (int i = 0; i < _mask.Length; i++)
+                {
+                    if (!Byte.TryParse(buffer[i], out res[i]))
+                    {
+                        throw new ArgumentException($"{nameof(NetworkMaskString)} has incorrect value", nameof(NetworkMaskString));
+                    }
+                }
+
+                _ip = res;
+            }
+        }
+
+        /// <summary>
+        /// Bit Mask - Network mask prefix
+        /// </summary>
+        public static int BitMask
+        {
+            get
+            {
+                string buffer = string.Empty;
+
+                foreach (var item in _mask)
+                {
+                    buffer += Convert.ToString(item, 2).PadLeft(OktetSize, '0');
+                }
+
+                return buffer.LastIndexOf('1') + 1;
+            }
+
+            set
+            {
+                if (value < 0 || value > DigitsAmountInIP)
+                {
+                    throw new ArgumentException($"{nameof(BitMask)} was incorrect", nameof(BitMask));
+                }
+
+                string buffer = string.Empty.PadLeft(value, '1');
+                buffer = buffer.PadRight(DigitsAmountInIP, '0');
+
+                for (int i = 0; i < buffer.Length; i += OktetSize)
+                {
+                    _mask[i / OktetSize] = Convert.ToByte(buffer.Substring(i, OktetSize), 2);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Return Wild Mask
+        /// </summary>
+        public static byte[] WildMask
+        {
+            get
+            {
+                byte[] res = new byte[_mask.Length];
+
+                string buffer;
+
+                for (int i = 0; i < _mask.Length; i++)
+                {
+                    buffer = Convert.ToString(~_mask[i], 2);
+                    buffer = buffer.Substring(sizeof(int) * OktetSize - OktetSize, OktetSize);
+
+                    res[i] = Convert.ToByte(buffer, 2);
+                }
+
+                return res;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(WildMask), $"{nameof(WildMask)} can't be null");
+                }
+                else if (value.Length != _mask.Length)
+                {
+                    throw new ArgumentException($"The inputed value length can't be different from the Mask length ({_mask.Length})", nameof(WildMask));
+                }
+
+                string buffer;
+
+                for (int i = 0; i < _mask.Length; i++)
+                {
+                    buffer = Convert.ToString(~value[i], 2);
+                    buffer = buffer.Substring(sizeof(int) * OktetSize - OktetSize, OktetSize);
+
+                    _mask[i] = Convert.ToByte(buffer, 2);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Return Wild Mask as string
+        /// </summary>
+        public static string WildMaskString
+        {
+            get
+            {
+                string res = string.Empty;
+
+                string buffer;
+
+                foreach (var item in _mask)
+                {
+                    buffer = Convert.ToString(~item, 2);
+                    buffer = buffer.Substring(sizeof(int) * OktetSize - OktetSize, OktetSize);
+
+                    res += Convert.ToByte(buffer, 2).ToString() + ".";
+                }
+
+                return res.Substring(0, res.Length - 1);
+            }
+        }
+
+        /// <summary>
+        /// Return Network Class
+        /// </summary>
+        public static string NetworkClass 
+        { 
+            get
+            {
+                string buffer = Convert.ToString(_ip[0], 2).PadLeft(OktetSize, '0');
+
+                if (buffer.StartsWith("0"))
+                {
+                    return "A";
+                }
+                else if (buffer.StartsWith("10"))
+                {
+                    return "B";
+                }
+                else if (buffer.StartsWith("110"))
+                {
+                    return "C";
+                }
+                else if (buffer.StartsWith("1110"))
+                {
+                    return "D";
+                }
+                else if (buffer.StartsWith("1111"))
+                {
+                    return "E";
+                }
+                else
+                {
+                    return "Unknown class";
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Return Broadcast Address as string
+        /// </summary>
+        public static string Broadcast 
+        { 
+            get
+            {
+                string res = string.Empty;
+
+                byte[] wildMask = WildMask;
+
+                byte[] ip = _ip;
+
+                for (int i = 0; i < ip.Length; i++)
+                {
+                    res += (ip[i] | wildMask[i]) + ".";
+                }
+
+                return res.Substring(0, res.Length - 1);
+            }
+
+        }
+
+        /// <summary>
+        /// Return Network Address as string
+        /// </summary>
+        public static string Address
+        {
+            get
+            {
+                string res = string.Empty;
+
+                for (int i = 0; i < _ip.Length; i++)
+                {
+                    res += (_ip[i] & _mask[i]) + ".";
+                }
+
+                return res.Substring(0, res.Length - 1);
+            }
+        }
+
+        /// <summary>
+        /// Return available Hosts Amount
+        /// </summary>
+        public static uint HostsAmount => (uint)(Math.Pow(2, DigitsAmountInIP - BitMask) - 2);
+
+        /// <summary>
+        /// Return First Network Address as string
+        /// </summary>
+        public static string FirstAddress
+        {
+            get
+            {
+                byte[] bufferIP = _ip;
+
+                IPString = Address;
+
+                IPInt += 1;
+
+                string res = IPString;
+
+                _ip = bufferIP;
+
+                return res;
+            }
+        }
+
+        /// <summary>
+        /// Return Last Network Address as string
+        /// </summary>
+        public static string LastAddress
+        {
+            get
+            {
+                
+                byte[] bufferIP = _ip;
+
+                IPString = Broadcast;
+
+                IPInt -= 1;
+
+                string res = IPString;
+
+                _ip = bufferIP;
+
+                return res;
+            }
+        }
+
+        /// <summary>
+        /// Return IP as int (if IP Version is 4) <br/>
+        /// Otherside - 0;
+        /// </summary>
+        public static int IPInt
+        {
+            get
+            {
+                if (_isIPv6)
+                {
+                    return 0;
+                }
+
+                string fullIP_d = string.Empty;
+
+                foreach (var item in IP)
+                {
+                    fullIP_d += Convert.ToString(item, 2).PadLeft(OktetSize, '0');
+                }
+
+                return Convert.ToInt32(fullIP_d, 2);
+            }
+
+            set
+            {
+                string buffer = Convert.ToString(value, 2).PadLeft(DigitsAmountInIP, '0');
+
+                for (int i = 0; i < buffer.Length; i += OktetSize)
+                {
+                    IP[i / OktetSize] = Convert.ToByte(buffer.Substring(i, OktetSize), 2);
+                }
+            }
+        }
+
+        /// <summary>
+        /// IP Address as string with spaces
+        /// </summary>
+        public static string IPStringWithSpaces
+        {
+            get
+            {
+                string res = string.Empty;
+
+                foreach (var item in _ip)
+                {
+                    res += item.ToString().PadRight(3, ' ') + ".";
+                }
+
+                return res.Substring(0, res.Length - 1);
+            }
+        }
+
+        #endregion
+    }
+}
