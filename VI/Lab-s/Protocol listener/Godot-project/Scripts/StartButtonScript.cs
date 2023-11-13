666736f636ebb652c8be61b140c88b725988634c
NemoNology <chicken.gang@mail.ru>
2023-11-13 12:46:36 +0900

    Added start/stop press handler

--- VI/Lab-s/Protocol listener/Godot-project/Scripts/StartButtonScript.cs
+++ VI/Lab-s/Protocol listener/Godot-project/Scripts/StartButtonScript.cs
@@ -3,5 +3,13 @@ using System;
 
 public partial class StartButtonScript : Button
 {
-    
+    public void OnStopPressed()
+    {
+        Disabled = false;
+    }
+
+    public void OnPressed()
+    {
+        Disabled = true;
+    }
 }
--- VI/Lab-s/Protocol listener/Godot-project/Scripts/StotButtonScript.cs
+++ VI/Lab-s/Protocol listener/Godot-project/Scripts/StotButtonScript.cs
@@ -3,4 +3,13 @@ using System;
 
 public partial class StotButtonScript : Button
 {
+    public void OnStartPressed()
+    {
+        Disabled = false;
+    }
+
+    public void OnPressed()
+    {
+        Disabled = true;
+    }
 }
