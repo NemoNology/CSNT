d4866eb4660b96125f62d1b5be7880f6ace8ce02
NemoNology <chicken.gang@mail.ru>
2023-11-13 12:45:36 +0900

    Removed extra code; TODO: Packets listening

--- VI/Lab-s/Protocol listener/Godot-project/Scripts/MainScript.cs
+++ VI/Lab-s/Protocol listener/Godot-project/Scripts/MainScript.cs
@@ -1,59 +1,35 @@
 using Godot;
-using System;
 using System.Net.Sockets;
-using System.Net;
 using System.Text;
+
 
 public partial class MainScript : Control
 {
     [Signal]
     public delegate void OnPacketGetEventHandler(string data);
 
-    Socket socket;
-    byte[] bytes = new byte[256];
-    bool isRunning = false;
-
     public override void _Ready()
     {
         base._Ready();
-        socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-        socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 65535));
-        socket.Listen(32);
     }
 
-    public override async void _Process(double delta)
+    public override void _Process(double delta)
     {
         base._Process(delta);
-
-        while (isRunning)
-        {
-            var listener = await socket.AcceptAsync();
-            var size = 0;
-            var data = new StringBuilder();
-
-            do
-            {
-                size = await listener.ReceiveAsync(bytes, SocketFlags.Peek);
-                data.Append(Encoding.UTF8.GetString(bytes, 0, size));
-            } while (listener.Available > 0);
-
-            EmitSignal(SignalName.OnPacketGet, data.ToString());
-        }
     }
 
     public void OnStopPressed()
     {
-        isRunning = false;
+
     }
 
     public void OnStartPressed()
     {
-        isRunning = true;
+
     }
 
     public override void _ExitTree()
     {
         base._ExitTree();
-        socket.Close();
     }
 }
