2f5db5c016838f9dccfc4b312549c0a4acd3d161
NemoNology <chicken.gang@mail.ru>
2023-11-13 12:46:57 +0900

    Fixed tabbing: Tab -> spaces

--- VI/Lab-s/Protocol listener/Godot-project/Scripts/PacketsViewScript.cs
+++ VI/Lab-s/Protocol listener/Godot-project/Scripts/PacketsViewScript.cs
@@ -2,8 +2,8 @@ using Godot;
 
 public partial class PacketsViewScript : ItemList
 {
-	public void OnPacketGet(string data)
-	{
-		AddItem(data);
-	}
+    public void OnPacketGet(string data)
+    {
+        AddItem(data);
+    }
 }
