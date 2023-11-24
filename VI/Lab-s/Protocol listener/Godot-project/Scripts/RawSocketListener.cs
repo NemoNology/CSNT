6739bc6e975fa1cb68f00138fd723b0500e8e6ec
NemoNology <chicken.gang@mail.ru>
2023-11-24 15:02:58 +0900

    Startem moved to not Raw packets getting; *
    * - Maybe I will return to raw packets soon

--- VI/Lab-s/Protocol listener/Godot-project/Scripts/RawSocketListener.cs
+++ VI/Lab-s/Protocol listener/Godot-project/Scripts/RawSocketListener.cs
@@ -4,44 +4,57 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-class RawSocketsListener
+class SocketsListener
 {
-    Socket socket = null!;
+    readonly Socket tcpSocket = null!;
+    readonly Socket udpSocket = null!;
     readonly IPEndPoint endPoint;
     public const int dataBufferLength = 2048;
     readonly byte[] dataBuffer = new byte[dataBufferLength];
     public List<byte[]> RawPackets { get; private set; }
-    public CancellationTokenSource CancellationTokenSource { get; private set; } = new CancellationTokenSource();
+    readonly CancellationTokenSource cancellationTokenSource = new();
     Task backgroundTask = null!;
 
-    public RawSocketsListener(IPAddress ip, int port)
+    public SocketsListener(IPAddress ip, int port)
     {
         endPoint = new(ip, port);
         RawPackets = new List<byte[]>(64);
-        socket = new(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);
-        socket.Bind(endPoint);
-        socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
+        tcpSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        udpSocket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        tcpSocket.Bind(endPoint);
+        udpSocket.Bind(endPoint);
+        tcpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
+        udpSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
     }
 
-    public void StartReciveAsync(bool clearAllPreviousPackets = true, bool isResizedPacketsAdding = true)
+    public void StartReciveAsync(bool clearAllPreviousPackets = true,
+                                bool isResizedPacketsAdding = true)
     {
         if (clearAllPreviousPackets)
             RawPackets.Clear();
 
         backgroundTask = Task.Run(() =>
         {
-            StartRecive(CancellationTokenSource.Token, false, isResizedPacketsAdding);
-        }, CancellationTokenSource.Token);
+            StartRecive(cancellationTokenSource.Token, false, isResizedPacketsAdding);
+        }, cancellationTokenSource.Token);
     }
 
-    public void StartRecive(CancellationToken cancellationToken, bool clearAllPreviousPackets = true, bool isResizedPacketsAdding = true)
+    public void StartRecive(CancellationToken cancellationToken,
+                            bool clearAllPreviousPackets = true,
+                            bool isResizedPacketsAdding = true)
     {
         if (clearAllPreviousPackets)
             RawPackets.Clear();
 
         while (!cancellationToken.IsCancellationRequested)
         {
-            int gettedDataSize = socket.Receive(dataBuffer);
+            int gettedDataSize = udpSocket.Receive(dataBuffer);
+            if (isResizedPacketsAdding)
+                RawPackets.Add(dataBuffer[..gettedDataSize]);
+            else
+                RawPackets.Add(dataBuffer);
+
+            gettedDataSize = tcpSocket.Receive(dataBuffer);
             if (isResizedPacketsAdding)
                 RawPackets.Add(dataBuffer[..gettedDataSize]);
             else
@@ -53,14 +66,22 @@ class RawSocketsListener
     {
         endPoint.Address = ip;
         endPoint.Port = port;
-        socket.Disconnect(true);
-        socket.Bind(endPoint);
+        tcpSocket.Disconnect(true);
+        udpSocket.Disconnect(true);
+        tcpSocket.Bind(endPoint);
+        udpSocket.Bind(endPoint);
     }
 
     public void StopRecieve()
     {
-        CancellationTokenSource.Cancel();
+        cancellationTokenSource.Cancel();
         while (!backgroundTask.IsCompleted)
             Task.Delay(10).Wait();
+    }
+
+    public async Task StopRecieveAsync()
+    {
+        cancellationTokenSource.Cancel();
+        await backgroundTask.WaitAsync(cancellationTokenSource.Token);
     }
 }
