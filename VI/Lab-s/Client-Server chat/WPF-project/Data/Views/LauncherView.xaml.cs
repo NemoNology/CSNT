7258a7a0fecd9f27fb9bf7e9310bcae91ff5e9ea
NemoNology <chicken.gang@mail.ru>
2023-12-21 11:04:28 +0900

    Removed logic

--- VI/Lab-s/Client-Server chat/WPF-project/Data/Views/LauncherView.xaml.cs
+++ VI/Lab-s/Client-Server chat/WPF-project/Data/Views/LauncherView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using System.Windows;
+﻿using System.Windows;
 
 namespace WPF_project.Data.Views
 {
@@ -8,26 +7,9 @@ namespace WPF_project.Data.Views
     /// </summary>
     public partial class Launcher : Window
     {
-        public bool IsLauncherClosingAfterProgramLaunching { get; set; }
-
-        private void OnLaunchClientClicked(object sender, RoutedEventArgs e)
-        {
-            new Client().Show();
-            CloseIfNeeded();
-        }
-
-        private void OnLaunchServerClicked(object sender, RoutedEventArgs e)
-        {
-            new Server().Show();
-            CloseIfNeeded();
-        }
-
-        private void CloseIfNeeded()
-        {
-            if (IsLauncherClosingAfterProgramLaunching)
-            {
-                Close();
-            }
-        }
+        public Launcher()
+		{
+			
+		}
     }
 }
