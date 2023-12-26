904ec018627223fff65f6ecc808ef8269f6c3382
NemoNology <chicken.gang@mail.ru>
2023-12-27 02:39:55 +0900

    (I)Server & (I)Client interfaces (now abstract classes):
    Changed:
    > Renamed
    > Reconstructed as abstract classes

--- VI/Lab-s/Client-Server chat/WPF-project/Data/Models/Interfaces/Client.cs
+++ VI/Lab-s/Client-Server chat/WPF-project/Data/Models/Interfaces/Client.cs
@@ -2,38 +2,41 @@
 
 namespace WPF_project.Data.Models.Interfaces
 {
-    public interface IClient
+    public abstract class Client
     {
         public static readonly byte[] ConnectionRequestBytesData = { 1 };
         public static readonly byte[] DisconnectNotificationBytesData = { 0 };
+        protected bool _isConnected = false;
+        protected CancellationTokenSource _source = null!;
         /// <summary>
         /// Raise when client received data
         /// </summary>
-        public event EventHandler<byte[]>? DataReceived;
+        public abstract event EventHandler<byte[]>? DataReceived;
         /// <summary>
         /// Raise when connection with remote host lost
         /// </summary>
-        public event EventHandler? ConnectionWithServerLost;
+        public abstract event EventHandler? ConnectionWithServerLost;
         /// <summary>
         /// Return <c>true</c> if client is connected to remote host;
         /// <c>false</c> - otherwise
         /// </summary>
-        public bool IsConnected { get; }
+        public bool IsConnected => _isConnected;
         /// <summary>
         /// Connect to remote host
         /// </summary>
         /// <param name="endPoint">Remote host IP-endpoint</param>
         /// <param name="connectionTime">Time given for connection</param>
         /// <returns><c>true</c> if connection was successful; <c>false</c> - otherwise</returns>
-        public bool Connect(IPEndPoint endPoint, TimeSpan connectionTime);
+        public abstract bool Connect(IPEndPoint endPoint, TimeSpan connectionTime);
         /// <summary>
         /// Disconnect from connected host
         /// </summary>
-        public void Disconnect();
+        public abstract void Disconnect();
+
         /// <summary>
         /// Send data as bytes to remote host
         /// </summary>
         /// <param name="bytes">Sending data</param>
-        public void SendBytes(byte[] bytes);
+        public abstract void SendData(byte[] bytes);
     }
 }
