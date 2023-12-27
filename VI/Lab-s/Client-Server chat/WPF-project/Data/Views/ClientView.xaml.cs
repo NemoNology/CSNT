ba7fe18ac8bfa4f5321f78d705de073d11f2cdc7
NemoNology <chicken.gang@mail.ru>
2023-12-27 17:04:17 +0900

    Completed client view - B 1.0

--- VI/Lab-s/Client-Server chat/WPF-project/Data/Views/ClientView.xaml.cs
+++ VI/Lab-s/Client-Server chat/WPF-project/Data/Views/ClientView.xaml.cs
@@ -1,15 +1,41 @@
 ﻿using System.Windows;
+using WPF_project.Data.Models.Interfaces;
 
 namespace WPF_project.Data.Views
 {
     /// <summary>
     /// Interaction logic for Client.xaml
     /// </summary>
-    public partial class Client : Window
+    public partial class ClientView : Window
     {
-        public Client()
+        public ClientView()
         {
             InitializeComponent();
+        }
+
+        private void OnClientsSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            viewModel.Client = (Client)clientInput.SelectedItem;
+        }
+
+        private void OnConnectClick(object sender, RoutedEventArgs e)
+        {
+            viewModel.Connect();
+        }
+
+        private void OnDisconnectClick(object sender, RoutedEventArgs e)
+        {
+            viewModel.Disconnect();
+        }
+
+        private void OnSendMessageClick(object sender, RoutedEventArgs e)
+        {
+            viewModel.SendMessage();
+        }
+
+        private void OnReturnClick(object sender, RoutedEventArgs e)
+        {
+            viewModel.ResetConnectionStatement();
         }
     }
 }
