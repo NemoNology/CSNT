cd2061e71027dcf024410c6604b73e255b5683bd
NemoNology <chicken.gang@mail.ru>
2023-12-28 16:02:31 +0900

    Client view-model:
    Added:
    + Client IP end point property
    Improved:
    ^ Fields & properties names

--- VI/Lab-s/Client-Server chat/WPF-project/Data/ViewModels/ClientViewModel.cs
+++ VI/Lab-s/Client-Server chat/WPF-project/Data/ViewModels/ClientViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Text;
+using System.Net;
 using System.Windows;
 using WPF_project.Data.Models.Implementations;
 using WPF_project.Data.Models.Interfaces;
@@ -10,6 +11,9 @@ namespace WPF_project.Data.ViewModels
     {
         private readonly Client[] _clients = { new ClientUDP() };
         private CancellationTokenSource _awaitingConnectionCancalletionSource = null!;
+        private readonly IPEndPoint _clientIPEndPoint = new(IPAddress.Loopback, 40405);
+        private string _clientIPAddressText = string.Empty;
+        private string _clientPortText = string.Empty;
         private Client _client = null!;
         private string _messageText = string.Empty;
         private string _username = "Некто";
@@ -21,6 +25,39 @@ namespace WPF_project.Data.ViewModels
             {
                 _client = value;
                 OnPropertyChanged(nameof(Client));
+            }
+        }
+        public string ClientIPAddressText
+        {
+            get => _clientIPAddressText;
+            set
+            {
+                _clientIPAddressText = value;
+                OnPropertyChanged(nameof(ClientIPAddressText));
+            }
+        }
+        public string ClientPortText
+        {
+            get => _clientPortText;
+            set
+            {
+                _clientPortText = value;
+                OnPropertyChanged(nameof(ClientPortText));
+            }
+        }
+        public bool IsClientIPAddressAndPortValid
+        {
+            get
+            {
+                var isIPAddressValid = IPAddress.TryParse(ClientIPAddressText, out var ipAddress);
+                var isPortValid = ushort.TryParse(ClientPortText, out var port);
+
+                if (isIPAddressValid)
+                    _clientIPEndPoint.Address = ipAddress!;
+                if (isPortValid)
+                    _clientIPEndPoint.Port = port!;
+
+                return isIPAddressValid && isPortValid;
             }
         }
         public bool? ConnectionStatement
@@ -44,9 +81,10 @@ namespace WPF_project.Data.ViewModels
             IsClientConnected || IsClientConnecting ? Visibility.Collapsed : Visibility.Visible;
         public bool EnabledOnMessageValid =>
             !string.IsNullOrWhiteSpace(MessageText);
-        public override bool IsInputParametersValid =>
+        public override bool AreInputParametersValid =>
             Client is not null
-            && IsIPAddressAndPortValid
+            && AreServerIPAddressAndPortValid
+            && IsClientIPAddressAndPortValid
             && !string.IsNullOrWhiteSpace(Username);
         public string Username
         {
@@ -69,8 +107,14 @@ namespace WPF_project.Data.ViewModels
         public string ConnectionStatementText =>
             _connectionStatement is null ? "Подключение..." : (
             (bool)_connectionStatement ?
-                "Успешно подключено" : "Не получилось подключиться;\nПерепроверьте входные данные и попытайтесь снова;"
+                "Успешно подключено" : "Не получилось подключиться/Потеряно соединение с сервером"
             );
+
+        public ClientViewModel() : base()
+        {
+            _clientIPAddressText = _clientIPEndPoint.Address.ToString();
+            _clientPortText = _clientIPEndPoint.Port.ToString();
+        }
 
         public void ResetConnectionStatement()
         {
@@ -83,17 +127,20 @@ namespace WPF_project.Data.ViewModels
         {
             if (e.PropertyName == nameof(Client))
             {
-                OnPropertyChanged(nameof(IsInputParametersValid));
+                OnPropertyChanged(nameof(AreInputParametersValid));
                 OnPropertyChanged(nameof(IsClientConnected));
                 Client.DataReceived += OnDataReceived;
+                Client.ConnectionWithServerLost += OnServerConnectionLost;
                 MessagesText = "";
             }
-            else if (e.PropertyName == nameof(IPAddressText)
-                || e.PropertyName == nameof(PortText))
+            else if (e.PropertyName == nameof(ServerIPAddressText)
+                || e.PropertyName == nameof(ServerPortText)
+                || e.PropertyName == nameof(ClientIPAddressText)
+                || e.PropertyName == nameof(ClientPortText))
             {
-                OnPropertyChanged(nameof(IsInputParametersValid));
+                OnPropertyChanged(nameof(AreInputParametersValid));
             }
-            else if (e.PropertyName == nameof(IsInputParametersValid))
+            else if (e.PropertyName == nameof(AreInputParametersValid))
             {
                 OnPropertyChanged(nameof(ErrorText));
             }
@@ -120,11 +167,19 @@ namespace WPF_project.Data.ViewModels
             }
         }
 
+        private void OnServerConnectionLost(object? sender, EventArgs e)
+        {
+            ConnectionStatement = false;
+        }
+        
         public async void Connect()
         {
             ConnectionStatement = null;
             _awaitingConnectionCancalletionSource = new();
-            ConnectionStatement = await Client.Connect(_ipEndPoint, _awaitingConnectionCancalletionSource.Token);
+            ConnectionStatement = await Client.Connect(
+                _clientIPEndPoint,
+                _serverIPEndPoint,
+                _awaitingConnectionCancalletionSource.Token);
         }
 
         public void Disconnect()
