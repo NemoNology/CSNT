9b7f9e4def67214758fc635b552eb6433e44378d
NemoNology <chicken.gang@mail.ru>
2024-04-23 12:04:03 +0900

    Client class
    Added
    + Is connected full property
    + Cancellation token source protected field
    Changed
    > Connect method delegate

--- VI/Lab-s/Client-Server chat/Godot-mono-project/Data/Models/Client.cs
+++ VI/Lab-s/Client-Server chat/Godot-mono-project/Data/Models/Client.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace CSNT.Clientserverchat.Data.Models
 {
     public abstract class Client
     {
         protected Socket _socket;
+        protected bool _isConnected = false;
+        protected readonly CancellationTokenSource _cancellationTokenSource = new();
 
         public abstract event Action<byte[]> MessageReceived;
 
-        public abstract void Connect(IPAddress ipAddress, int port);
+        public bool IsConnected => _isConnected;
+
+        public abstract void Connect(IPAddress clientIpAddress, int clientPort, IPAddress serverIpAddress, int serverPort);
         public abstract void Dissconnect();
         public abstract void SendMessage(string message);
     }
