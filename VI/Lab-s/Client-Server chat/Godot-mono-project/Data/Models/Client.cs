53bee0742ad927e7ac3028899e1b6bb420b54dda
NemoNology <chicken.gang@mail.ru>
2024-04-23 15:05:24 +0900

    Client class
    Added
    + IDisposable interface implementation

--- VI/Lab-s/Client-Server chat/Godot-mono-project/Data/Models/Client.cs
+++ VI/Lab-s/Client-Server chat/Godot-mono-project/Data/Models/Client.cs
@@ -5,10 +5,11 @@ using System.Threading;
 
 namespace CSNT.Clientserverchat.Data.Models
 {
-    public abstract class Client
+    public abstract class Client : IDisposable
     {
         protected Socket _socket;
         protected bool _isConnected = false;
+        protected bool _isDisposed = false;
         protected readonly CancellationTokenSource _cancellationTokenSource = new();
 
         public abstract event Action<byte[]> MessageReceived;
@@ -16,7 +17,20 @@ namespace CSNT.Clientserverchat.Data.Models
         public bool IsConnected => _isConnected;
 
         public abstract void Connect(IPAddress clientIpAddress, int clientPort, IPAddress serverIpAddress, int serverPort);
-        public abstract void Dissconnect();
+        public abstract void Disconnect();
         public abstract void SendMessage(string message);
+
+        public void Dispose()
+        {
+            if (_isDisposed)
+                return;
+
+            Disconnect();
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Dispose();
+            _cancellationTokenSource.Dispose();
+            GC.SuppressFinalize(this);
+            _isDisposed = true;
+        }
     }
 }
