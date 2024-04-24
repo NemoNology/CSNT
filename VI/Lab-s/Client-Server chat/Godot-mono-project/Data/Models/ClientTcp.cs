572bae590b0a06392f7917c42abb85a3214e95b3
NemoNology <chicken.gang@mail.ru>
2024-04-24 12:41:33 +0900

    Client Tcp
    Fixed
    * Client state not changing after connecting. State changed with `_isConnected` field
    * Client state no change after dissconect. Now state changing starting earlier for not invoking errors
    Added
    + Server stopping handler
    + Check-ups for socket state before interactions with socket

--- VI/Lab-s/Client-Server chat/Godot-mono-project/Data/Models/ClientTcp.cs
+++ VI/Lab-s/Client-Server chat/Godot-mono-project/Data/Models/ClientTcp.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -8,6 +9,7 @@ namespace CSNT.Clientserverchat.Data.Models
 {
     public class ClientTcp : Client
     {
+        // private Socket _connectedSocket;
         public override event Action<byte[]> MessageReceived;
 
         public ClientTcp()
@@ -16,7 +18,6 @@ namespace CSNT.Clientserverchat.Data.Models
             _socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
         }
 
-
         public override void Connect(IPAddress clientIpAddress, int clientPort, IPAddress serverIpAddress, int serverPort)
         {
             if (_isConnected)
@@ -24,32 +25,48 @@ namespace CSNT.Clientserverchat.Data.Models
 
             _socket.Bind(new IPEndPoint(clientIpAddress, clientPort));
             _socket.Connect(new IPEndPoint(serverIpAddress, serverPort));
-            Task.Run(() =>
+            _isConnected = true;
+            // Thread for connection and recieving messages
+            Task.Run(async () =>
             {
                 byte[] buffer = new byte[4096];
+                // Start listening (Awaiting connection) and accept connection
+                // _socket.Listen(1);
+                // _connectedSocket = _socket.Accept();
+                // Start recieve messages
                 while (_isConnected)
                 {
-                    var recievedBytesLength = _socket.Receive(buffer);
-                    MessageReceived?.Invoke(buffer[..recievedBytesLength]);
+                    // var recievedBytesLength = _connectedSocket.Receive(buffer);
+                    var recievedBytesLength = await _socket.ReceiveAsync(
+                        buffer, SocketFlags.None, _cancellationTokenSource.Token);
+                    byte[] recievedBytes = buffer[..recievedBytesLength];
+                    MessageReceived?.Invoke(recievedBytes);
+                    // If message is empty - server clising, so we need to disconnect
+                    if (Enumerable.SequenceEqual(recievedBytes, ServerTcp.CloseMessageBytes))
+                    {
+                        Disconnect();
+                        return;
+                    }
                 }
             }, _cancellationTokenSource.Token);
         }
 
-        public override void Dissconnect()
+        public override async void Disconnect()
         {
-            if (!_isConnected)
+            if (!_isConnected || _isDisposed)
                 return;
 
-            _socket.Disconnect(true);
             _isConnected = false;
+            await _socket.DisconnectAsync(true);
             _cancellationTokenSource.Cancel();
         }
 
         public override void SendMessage(string message)
         {
-            if (!_isConnected)
+            if (!_isConnected || _isDisposed || !_socket.Connected)
                 return;
 
+            // _connectedSocket.Send(Encoding.UTF8.GetBytes(message));
             _socket.Send(Encoding.UTF8.GetBytes(message));
         }
     }
