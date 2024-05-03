570291664aa5f6a25ba163c475ca78435a9be904
NemoNology <chicken.gang@mail.ru>
2024-05-03 13:29:50 +0900

    Added main scene
    Added
    + GUI
    + Input values validation
    + Scene changing

---
+++ VI/Lab-s/Protocol listener/Godot-mono-project/Data/Controllers/MainViewController.cs
@@ -0,0 +1,37 @@
+using System.Net;
+using Godot;
+using LacpSniffer.Models;
+namespace LacpSniffer.Controllers;
+
+public partial class MainViewController : Control
+{
+    [Export]
+    public LineEdit IpAddressInput { get; set; }
+    [Export]
+    public LineEdit PortInput { get; set; }
+    [Export]
+    public Label ErrorsOutput { get; set; }
+
+    public void OnStartSnifferingButtonPressed()
+    {
+        ushort port = 0;
+        if (!IPAddress.TryParse(IpAddressInput.Text, out IPAddress address))
+        {
+            ErrorsOutput.Text = "Некорректный IP-адрес;";
+        }
+        else if (!ushort.TryParse(PortInput.Text, out port))
+        {
+            ErrorsOutput.Text += "\nНекорректный порт;";
+            return;
+        }
+        else if (SnifferController.IsAddressBusy(address, port))
+        {
+            ErrorsOutput.Text = "Данный адрес (IP + порт) занят;";
+        }
+
+        ErrorsOutput.Text = "";
+        SnifferController.IpEndPoint = new(address, port);
+
+        GetTree().ChangeSceneToFile("res://Data/Scenes/sniffering.tscn");
+    }
+}
