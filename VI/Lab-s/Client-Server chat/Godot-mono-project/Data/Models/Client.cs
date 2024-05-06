b665788dd1c80bcbf3b61cee233244f437bee571
NemoNology <chicken.gang@mail.ru>
2024-05-07 04:42:34 +0900

    Client abstract class
    Removed
    - IDisposable interface implementation
    - 'Socket' protected field, because I now using specified classes (UdpClient & TcpClient) instead of Socket class

--- VI/Lab-s/Client-Server chat/Godot-mono-project/Data/Models/Client.cs
+++ VI/Lab-s/Client-Server chat/Godot-mono-project/Data/Models/Client.cs
@@ -1,15 +1,12 @@
 using System;
 using System.Net;
-using System.Net.Sockets;
 using System.Threading;
 
 namespace CSNT.Clientserverchat.Data.Models
 {
-    public abstract class Client : IDisposable
+    public abstract class Client
     {
-        protected Socket _socket;
         protected bool _isConnected = false;
-        protected bool _isDisposed = false;
         protected readonly CancellationTokenSource _cancellationTokenSource = new();
 
         public abstract event Action<byte[]> MessageReceived;
@@ -19,18 +16,5 @@ namespace CSNT.Clientserverchat.Data.Models
         public abstract void Connect(IPAddress clientIpAddress, int clientPort, IPAddress serverIpAddress, int serverPort);
         public abstract void Disconnect();
         public abstract void SendMessage(string message);
-
-        public void Dispose()
-        {
-            if (_isDisposed)
-                return;
-
-            Disconnect();
-            _socket.Shutdown(SocketShutdown.Both);
-            _socket.Dispose();
-            _cancellationTokenSource.Dispose();
-            GC.SuppressFinalize(this);
-            _isDisposed = true;
-        }
     }
 }
