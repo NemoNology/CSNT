98ab76e01b17a1a5b785df523548390a982ed931
NemoNology <chicken.gang@mail.ru>
2024-05-10 06:27:21 +0900

    United many scenes in one

--- VI/Lab-s/Protocol listener/Godot-mono-project/Data/Controllers/MainViewController.cs
+++ VI/Lab-s/Protocol listener/Godot-mono-project/Data/Controllers/MainViewController.cs
@@ -1,37 +1,67 @@
+using System;
+using System.Linq;
 using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
 using Godot;
 using LacpSniffer.Data.Models;
+using LACPsniffer.Data.Models;
 namespace LacpSniffer.Data.Controllers;
 
 public partial class MainViewController : Control
 {
-    [Export]
-    public LineEdit IpAddressInput { get; set; }
+    private readonly CancellationTokenSource _cancellationTokenSource = new();
+    private Socket _socket;
+    private bool _isListening = false;
+
+    [ExportCategory("Sniffer")]
     [Export]
     public LineEdit PortInput { get; set; }
     [Export]
+    public ItemList PacketsOutput { get; set; }
+    [Export]
     public Label ErrorsOutput { get; set; }
+    [Export]
+    public Button StartStopListenningButton { get; set; }
 
-    public void OnStartSnifferingButtonPressed()
+    public const int BUFFERSIZE = 4096;
+
+    private void OnStartStopSnifferingButtonPressed()
     {
-        ushort port = 0;
-        if (!IPAddress.TryParse(IpAddressInput.Text, out IPAddress address))
-        {
-            ErrorsOutput.Text = "Некорректный IP-адрес;";
-        }
-        else if (!ushort.TryParse(PortInput.Text, out port))
-        {
-            ErrorsOutput.Text += "\nНекорректный порт;";
-            return;
-        }
-        else if (SnifferController.IsAddressBusy(address, port))
-        {
-            ErrorsOutput.Text = "Данный адрес (IP + порт) занят;";
-        }
-
         ErrorsOutput.Text = "";
-        SnifferController.IPEndPoint = new(address, port);
+        if (_isListening)
+        {
+            _cancellationTokenSource.Cancel();
+            _socket.Close();
+        }
+        else
+        {
+            if (!ushort.TryParse(PortInput.Text, out var port))
+            {
+                ErrorsOutput.Text = "Некорректный порт";
+                return;
+            }
+            PacketsOutput.Clear();
+            _socket = new(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Raw);
+            Task.Run(() =>
+            {
+                EndPoint endPoint;
+                byte[] buffer = new byte[BUFFERSIZE];
+                while (!_cancellationTokenSource.IsCancellationRequested)
+                {
+                    endPoint = new IPEndPoint(IPAddress.Any, port);
+                    int receivedBytesLength = _socket.ReceiveFrom(buffer, BUFFERSIZE, SocketFlags.None, ref endPoint);
+                    GD.Print($"Recieved bytes:\n\t{string.Join(", ", buffer[..receivedBytesLength].Select(x => x.ToString()))}");
+                }
+            }, _cancellationTokenSource.Token);
+        }
+        _isListening = !_isListening;
+        StartStopListenningButton.Text = (_isListening ? "Начать" : "Закончить") + " слушать";
+    }
 
-        GetTree().ChangeSceneToFile("res://Data/Views/sniffering.tscn");
+    private void OnPacketRecieved(LacpPacket packet)
+    {
+        PacketsOutput.AddItem(packet.ToString());
     }
 }
