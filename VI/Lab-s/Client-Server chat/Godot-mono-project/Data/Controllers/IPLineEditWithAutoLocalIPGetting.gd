24f709c80ec2e01d2a2d450b9307057ca427a1bd
NemoNology <chicken.gang@mail.ru>
2024-05-10 02:35:26 +0900

    Removed extra script

--- VI/Lab-s/Client-Server chat/Godot-mono-project/Data/Controllers/IPLineEditWithAutoLocalIPGetting.gd
+++
@@ -1,9 +0,0 @@
-extends LineEdit
-
-func _ready():
-	text = "192.168.0.0"
-	var interfaces = IP.get_local_interfaces()
-	for dict in interfaces:
-		if dict["friendly"] == "Ethernet":
-			text = dict["addresses"][1]
-			return
