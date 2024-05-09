6d4c3252816c041113ac9cd621e69e350c0015bb
NemoNology <chicken.gang@mail.ru>
2024-05-09 21:24:29 +0900

    Server view controller
    Changed
    > Changing UI now by `CallDeffered` methods, not by `lock`;
    > Messages output from vertical box of labels to label;
    Added
    + Server states changing handler;

--- VI/Lab-s/Client-Server chat/Godot-mono-project/Data/Controllers/ServerViewController.cs
+++ VI/Lab-s/Client-Server chat/Godot-mono-project/Data/Controllers/ServerViewController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Net;
 using System.Text;
 using CSNT.Clientserverchat.Data.Models;
@@ -24,8 +25,7 @@ namespace CSNT.Clientserverchat.Data.Controllers
         [Export]
         public Label ErrorsOutput { get; set; }
         [Export]
-        public VBoxContainer MessagesContainer { get; set; }
-
+        public Label MessagesOutput { get; set; }
 
         private void OnRunServerButtonPressed()
         {
@@ -33,63 +33,81 @@ namespace CSNT.Clientserverchat.Data.Controllers
             bool isUdp = ProtocolInput.Selected == 0;
             if (!IPAddress.TryParse(IpAddressInput.Text, out IPAddress ipAddress))
             {
-                CallDeferred(nameof(PrintErrorMessage), "Неверный IP-адрес");
+                PrintErrorMessage("Неверный IP-адрес");
                 return;
             }
             else if (!ushort.TryParse(PortInput.Text, out port))
             {
-                CallDeferred(nameof(PrintErrorMessage), "Неверный порт");
+                PrintErrorMessage("Неверный порт");
                 return;
             }
             else if (!NetHelper.IsAddressForTransportProtocolAvailable(new IPEndPoint(ipAddress, port), isUdp))
             {
-                CallDeferred(nameof(PrintErrorMessage), "Данный адрес занят");
+                PrintErrorMessage("Данный адрес занят");
                 return;
             }
 
-            CallDeferred(nameof(PrintErrorMessage), "");
+            PrintErrorMessage("");
             _server = isUdp ? new ServerUdp() : new ServerTcp();
-            _server.MessageReceived += OnMessageRecieved;
+            _server.MessageReceived += OnMessageRecievedDeferred;
+            _server.StateChanged += OnServerStateChangedDeferred;
 
             try
             {
                 _server.Start(ipAddress, port);
-                CallDeferred(nameof(SwitchControlsVisibility));
             }
             catch (Exception e)
             {
-                CallDeferred(nameof(PrintErrorMessage), "Не удалось запустить сервер:\n" + e.Message);
+                PrintErrorMessageDeferred("Не удалось запустить сервер:\n" + e.Message);
+            }
+        }
+
+        private void PrintErrorMessageDeferred(string v)
+        {
+            CallDeferred(nameof(PrintErrorMessage), v);
+        }
+
+        private void OnServerStateChangedDeferred(bool isRunning)
+        {
+            CallDeferred(nameof(OnServerStateChanged), isRunning);
+        }
+
+        private void OnMessageRecievedDeferred(byte[] messageBytes)
+        {
+            CallDeferred(nameof(AddMessageToOutput), messageBytes);
+        }
+
+        private void OnServerStateChanged(bool state)
+        {
+            switch (state)
+            {
+                case true:
+                    ServerRunningControl.Visible = true;
+                    ServerNotRunningControl.Visible = false;
+                    return;
+                case false:
+                    ServerRunningControl.Visible = false;
+                    ServerNotRunningControl.Visible = true;
+                    return;
             }
         }
 
         private void OnStopServerButtonPressed()
         {
             _server.Stop();
-            _server.MessageReceived -= OnMessageRecieved;
+            _server.MessageReceived -= OnMessageRecievedDeferred;
+            _server.StateChanged -= OnServerStateChangedDeferred;
             CallDeferred(nameof(ClearMessages));
-            CallDeferred(nameof(SwitchControlsVisibility));
-        }
-
-        private void SwitchControlsVisibility()
-        {
-            ServerRunningControl.Visible = !ServerRunningControl.Visible;
-            ServerNotRunningControl.Visible = !ServerNotRunningControl.Visible;
         }
 
         private void ClearMessages()
         {
-            foreach (Node child in MessagesContainer.GetChildren())
-                MessagesContainer.RemoveChild(child);
+            MessagesOutput.Text = "";
         }
 
-        private void OnMessageRecieved(byte[] messageBytes)
+        private void AddMessageToOutput(byte[] messageBytes)
         {
-            CallDeferred(nameof(AddMessageAsChild), messageBytes);
-        }
-
-        private void AddMessageAsChild(byte[] messageBytes)
-        {
-            MessagesContainer.AddChild(new Label { Text = Encoding.UTF8.GetString(messageBytes) });
+            MessagesOutput.Text += Encoding.UTF8.GetString(messageBytes);
         }
 
         private void PrintErrorMessage(string message)
