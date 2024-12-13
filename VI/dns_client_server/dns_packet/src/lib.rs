5293da04e5e4307b58cd3826f997c12c9064d98a
NemoNology <nemonology@mail.ru>
2024-12-14 01:48:30 +0900

    Added comments and improved naming of one variable

--- VI/dns_client_server/dns_packet/src/lib.rs
+++ VI/dns_client_server/dns_packet/src/lib.rs
@@ -1,28 +1,36 @@
 use std::net::Ipv4Addr;
 
 pub enum DnsRecordResponse {
+    /// Resolved address of domain name
     Address(Ipv4Addr),
+    /// Domain name of name server, which contains address of resolving domain name
     NameServer(String),
+    /// Address or name server of resolving domain name not found
     NotFound,
 }
 
-pub enum DnsRecord {
+pub enum DnsData {
+    /// Request to resolve domain name
     Query(String),
+    /// Answer to resolving domain name request
     Response(DnsRecordResponse),
 }
 
 pub struct DnsPacket {
+    /// Transaction ID
     pub id: u16,
+    /// Name server, which contains address of resolving domain name
     pub name_server: String,
-    pub record: DnsRecord,
+    /// Transaction data (query or answer)
+    pub data: DnsData,
 }
 
 impl DnsPacket {
-    pub fn new(id: u16, name_server: String, record: DnsRecord) -> Self {
+    pub fn new(id: u16, name_server: String, data: DnsData) -> Self {
         DnsPacket {
             id,
             name_server,
-            record,
+            data,
         }
     }
 }
