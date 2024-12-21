3364b24dd376001f7f7591ef0683452e4de06978
NemoNology <nemonology@mail.ru>
2024-12-21 11:20:53 +0900

    Added DNS client console app

--- VI/dns/client/src/main.rs
+++ VI/dns/client/src/main.rs
@@ -1,3 +1,25 @@
+use clap::{arg, Parser};
+use dns::client::DnsClient;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Path to hosts file
+    #[arg(short, default_value = r"C:\Users\NemoNology\hosts.txt")]
+    path: String,
+    /// Resolving domain name
+    #[arg(short)]
+    domain_name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let client = DnsClient::new(&args.path).expect("Can't init DNS client");
+
+    match client.resolve(&args.domain_name) {
+        Ok(address_option) => match address_option {
+            Some(address) => println!("Resolved address: {}", address),
+            None => println!("Address not resolved"),
+        },
+        Err(e) => eprint!("Error: {}", e),
+    }
 }
