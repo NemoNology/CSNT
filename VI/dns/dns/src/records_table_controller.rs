78e73b696094397791fab19a585b54c6b9796c77
NemoNology <nemonology@mail.ru>
2024-12-26 07:51:44 +0900

    DNS record table controller
    Changed
    > struct type to tuple struct;
    > errors structs moved to pub submodule;

--- VI/dns/dns/src/records_table_controller.rs
+++ VI/dns/dns/src/records_table_controller.rs
@@ -1,67 +1,67 @@
 use std::error::Error;
-use std::fmt;
 use std::fs::{File, OpenOptions};
-use std::io::{prelude::Write, BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 
-use crate::record::errors::DnsRecordParseError;
+use errors::DnsRecordsTableParseError;
+
 use crate::record::DnsRecord;
 
-#[derive(Debug, Clone)]
-pub struct DnsRecordsTableParseError {
-    pub line_number: usize,
-    pub inner_error: DnsRecordParseError,
-}
+pub struct DnsRecordsTableController(String);
 
-impl fmt::Display for DnsRecordsTableParseError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "Unexpected DNS record at line {}. {}",
-            self.line_number, self.inner_error
-        )
-    }
-}
-
-impl Error for DnsRecordsTableParseError {}
-
-pub struct DnsRecordsTableController<'a> {
-    pub records_table_path: &'a str,
-}
-
-impl<'a> DnsRecordsTableController<'a> {
-    pub fn new(records_table_path: &'a str) -> Self {
-        DnsRecordsTableController { records_table_path }
+impl DnsRecordsTableController {
+    pub fn new(records_table_path: String) -> Self {
+        DnsRecordsTableController(records_table_path)
     }
 
     pub fn get_records_list(&self) -> Result<Vec<DnsRecord>, Box<dyn Error>> {
-        let file = File::open(&self.records_table_path)?;
-        let reader = BufReader::new(file);
+        let file = File::open(&self.0)?;
         let mut records: Vec<DnsRecord> = Vec::new();
-        let mut line_number: usize = 0;
 
-        for line in reader.lines() {
+        // Read lines with their line number
+        for (line_number, line) in BufReader::new(file).lines().enumerate() {
             let line = line?;
-            line_number += 1;
-            let dns_record = DnsRecord::try_from(line.as_str());
-            if let Err(inner_error) = dns_record {
-                return Err(Box::new(DnsRecordsTableParseError {
-                    line_number,
-                    inner_error,
-                }));
-            }
-
-            records.push(dns_record.unwrap());
+            // Handle errors/Parse record and push it
+            records.push(match DnsRecord::build(line.as_str()) {
+                Ok(record) => record,
+                Err(inner_error) => {
+                    return Err(Box::new(DnsRecordsTableParseError {
+                        line_number,
+                        inner_error,
+                    }))
+                }
+            });
         }
 
         Ok(records)
     }
 
     pub fn add_record_to_table(&self, record: &DnsRecord) -> Result<(), Box<dyn Error>> {
-        let mut file = OpenOptions::new()
-            .write(true)
-            .append(true)
-            .open(&self.records_table_path)?;
+        let mut file = OpenOptions::new().write(true).append(true).open(&self.0)?;
         writeln!(file, "{} {}", record.domain_name, record.address)?;
         Ok(())
     }
+}
+
+pub mod errors {
+    use std::{error::Error, fmt};
+
+    use crate::record::errors::DnsRecordParseError;
+
+    #[derive(Debug, Clone)]
+    pub struct DnsRecordsTableParseError {
+        pub line_number: usize,
+        pub inner_error: DnsRecordParseError,
+    }
+
+    impl fmt::Display for DnsRecordsTableParseError {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(
+                f,
+                "Unexpected DNS record at line {}. {}",
+                self.line_number, self.inner_error
+            )
+        }
+    }
+
+    impl Error for DnsRecordsTableParseError {}
 }
