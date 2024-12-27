a0158aef855360dda3d7acc8388014d82e5c347d
NemoNology <nemonology@mail.ru>
2024-12-28 08:52:49 +0900

    DNS client
    Changed
    > constructor params;
    > resolve method split into pub methods;
    Added
    + server resolving test;

--- VI/dns/dns/src/client.rs
+++ VI/dns/dns/src/client.rs
@@ -2,21 +2,27 @@ use std::{
     error::Error,
     fs::File,
     io::{BufRead, BufReader},
-    net::{Ipv4Addr, UdpSocket},
+    net::{Ipv4Addr, SocketAddrV4, UdpSocket},
+    time::Duration,
 };
 
-use crate::{packet::DnsPacket, server::DnsServer};
+use crate::packet::DnsPacket;
 
 pub struct DnsClient<'a> {
-    pub udp_socket: UdpSocket,
+    udp_socket: UdpSocket,
     hosts_path: &'a str,
 }
 
 impl<'a> DnsClient<'a> {
-    pub fn new(hosts_path: &'a str) -> Result<Self, Box<dyn Error>> {
+    /// set `server_wait_timeout_secs` to `0` for endless server answer wait.
+    pub fn new(
+        hosts_path: &'a str,
+        server_port: u16,
+        server_wait_timeout_secs: u64,
+    ) -> Result<Self, Box<dyn Error>> {
         let udp_socket = UdpSocket::bind("127.0.0.1:0")?;
-        let server_address = format!("127.0.0.1:{}", DnsServer::UDP_LISTENING_PORT);
-        udp_socket.connect(&server_address)?;
+        udp_socket.connect(SocketAddrV4::new(Ipv4Addr::LOCALHOST, server_port))?;
+        udp_socket.set_read_timeout(Some(Duration::new(server_wait_timeout_secs, 0)))?;
 
         Ok(DnsClient {
             udp_socket,
@@ -24,16 +30,7 @@ impl<'a> DnsClient<'a> {
         })
     }
 
-    pub fn resolve<'b>(&self, domain_name: &'b str) -> Result<Option<Ipv4Addr>, Box<dyn Error>> {
-        let mut answer = self.resolve_in_hosts(domain_name);
-        if answer.is_err() {
-            answer = self.resolve_in_server(domain_name.to_string());
-        }
-
-        answer
-    }
-
-    fn resolve_in_hosts<'b>(
+    pub fn resolve_in_hosts<'b>(
         &self,
         domain_name: &'b str,
     ) -> Result<Option<Ipv4Addr>, Box<dyn Error>> {
@@ -47,34 +44,35 @@ impl<'a> DnsClient<'a> {
                 continue;
             }
 
+            // split line to address & domain name
             let mut parts = line.split_ascii_whitespace();
-            let found_address = parts.next().unwrap();
-            let found_domain_name = parts.next();
+            // get address, when line contains both parts
+            let (address, name) = match (parts.next(), parts.next()) {
+                (Some(address), Some(name)) => (address, name),
+                _ => continue,
+            };
 
-            if found_domain_name.is_none() || found_domain_name.unwrap() != domain_name {
-                continue;
+            if domain_name == name {
+                return Ok(Some(address.parse()?));
             }
-
-            let address: Ipv4Addr = found_address.parse()?;
-            return Ok(Some(address));
         }
 
         Ok(None)
     }
 
-    fn resolve_in_server<'b>(
-        &self,
-        domain_name: String,
-    ) -> Result<Option<Ipv4Addr>, Box<dyn Error>> {
+    pub fn send_dns_query(&self, domain_name: String) -> Result<(), Box<dyn Error>> {
         let packet = DnsPacket::init(domain_name.len() as u8, domain_name);
         let bytes: Vec<u8> = packet.into_bytes();
         self.udp_socket.send(&bytes)?;
-        let mut bytes_buffer = [42; 264];
+        Ok(())
+    }
 
+    pub fn wait_server_answer_packet(&self) -> Result<DnsPacket, Box<dyn Error>> {
+        let mut bytes_buffer = [0; DnsPacket::MAX_PACKET_LENGTH];
         let answer_size = self.udp_socket.recv(&mut bytes_buffer)?;
         let answer_packet = DnsPacket::try_from(&bytes_buffer[..answer_size])?;
 
-        Ok(answer_packet.resolved_address)
+        Ok(answer_packet)
     }
 }
 
@@ -84,10 +82,9 @@ mod tests {
 
     #[test]
     fn hosts_resolving() {
-        let path = r"C:\Users\NemoNology\hosts.txt";
-        let client = DnsClient::new(path).unwrap();
+        let client = DnsClient::new(r"C:\Users\NemoNology\hosts.txt", 60053, 2).unwrap();
 
-        let domain_names: [&str; 4] = ["test1.com", "oizi.dms.zab", "ya.ru", "wiki.local"];
+        let domain_names: [&str; 4] = ["test1.com", "oizi.sud.net", "yo.ru", "wiki.local"];
         let addresses: [Ipv4Addr; 4] = [
             Ipv4Addr::new(55, 44, 66, 55),
             Ipv4Addr::new(10, 11, 3, 139),
@@ -96,8 +93,36 @@ mod tests {
         ];
 
         for (name, address) in domain_names.iter().zip(addresses) {
-            let resolved_address = client.resolve(name).unwrap().unwrap();
+            let resolved_address = client.resolve_in_hosts(name).unwrap().unwrap();
             assert_eq!(resolved_address, address);
+        }
+    }
+
+    /// Requires started server
+    #[test]
+    fn server_resolving() {
+        let domain_names: [&str; 6] = [
+            "ya.ru",
+            "oizi.dms.zab",
+            "paint.local",
+            "rutube.ru",
+            "работа.рф",
+            "wiki.net",
+        ];
+        let addresses: [Ipv4Addr; 6] = [
+            Ipv4Addr::new(44, 55, 66, 55),
+            Ipv4Addr::new(10, 11, 3, 139),
+            Ipv4Addr::new(127, 0, 1, 25),
+            Ipv4Addr::new(44, 55, 44, 55),
+            Ipv4Addr::new(55, 55, 55, 55),
+            Ipv4Addr::new(11, 12, 13, 14),
+        ];
+
+        let client = DnsClient::new("", 60053, 2).unwrap();
+        for (name, addr) in domain_names.iter().zip(addresses) {
+            let _ = client.send_dns_query(name.to_string());
+            let answer = client.wait_server_answer_packet().unwrap();
+            assert_eq!(answer.resolved_address.unwrap(), addr);
         }
     }
 }
